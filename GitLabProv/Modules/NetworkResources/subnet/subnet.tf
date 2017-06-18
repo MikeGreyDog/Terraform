8a1209b8c87fb25066fbdabc0c0cc73a45a4523c
TestUserLp <orest.a.lavriv@lpnu.ua>
2017-06-18 13:26:24 +0300

    delete old

--- GitLabProv/Modules/NetworkResources/subnet/subnet.tf
+++
@@ -1,9 +0,0 @@
-# Create a subnet in our new network
-# Notice here we use a TF variable for the name of our network above.
-#
-resource "openstack_networking_subnet_v2" "tf_net_sub1" {
-  region = ""
-  network_id = "${openstack_networking_network_v2.tf_network.id}"
-  cidr = "192.168.103.128/25"
-  ip_version = 4
-}
