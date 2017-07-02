16a549a079ce91853a03f1f57ded85896c663215
TestUserLp <orest.a.lavriv@lpnu.ua>
2017-07-02 15:26:50 +0300

    add Zabbix

--- ciInfrastructure/Stack/Zabbix/main.tf
+++ ciInfrastructure/Stack/Zabbix/main.tf
@@ -11,20 +11,12 @@ variable "flav_name" {}
 variable "keypair_name" {}
 variable "sec_group_name" {}
 variable "net_id" {}
-variable "device_id" {}
-variable "source_name" {}
-variable "size" {}
-variable "index" {}
-variable "dest_type" {}
 variable "keypair_value" {}
-#floatingIP
-//variable "pool_name" {}
-#floatingIPassociate
-//variable "address" {}
-//variable "instance_identifier" {}
-//variable "ansible_command" {}
+variable "cnt" {}
 variable "ans_ssh_user" {}
 variable "ans_ssh_pass" {}
+variable "server_name" {}
+variable "playbook" {}
 
 provider "openstack" {
   user_name = "${var.openstack_name}"
@@ -40,12 +32,6 @@ module "keypair" {
   region_name = "${var.region_name}"
 }
 
-/*module "floatingIP" {
-  source = "../../Modules/ComputeResources/floatingIP"
-  region_name = "${var.region_name}"
-  pool_name = "${var.pool_name}"
-}*/
-
 module "instance" {
   source = "../../Modules/ComputeResources/instance"
   instance_name = "${var.instance_name}"
@@ -55,20 +41,10 @@ module "instance" {
   keypair_name = "${module.keypair.keypair_name_value}"
   sec_group_name = "${var.sec_group_name}"
   net_id = "${var.net_id}"
-  device_id = "${var.device_id}"
-  source_name = "${var.source_name}"
-  size = "${var.size}"
-  index = "${var.index}"
-  dest_type = "${var.dest_type}"
-  //ansible_command = "${var.ansible_command}"
+  cnt = "${var.cnt}"
   ans_ssh_user = "${var.ans_ssh_user}"
   ans_ssh_pass = "${var.ans_ssh_pass}"
+  server_name = "${var.server_name}"
+  playbook = "${var.playbook}"
 
 }
-
-/*module "floatingIPassociate" {
-  source = "../../Modules/ComputeResources/floatingIPassociate"
-  region_name = "${var.region_name}"
-  address = "${module.floatingIP.fip1_computed_address}"
-  instance_identifier = "${module.instance.instance_identifier}"
-}*/
